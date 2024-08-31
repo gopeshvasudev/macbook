afcd54d3c90d63d316306b4b7045414780132a4a
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-08-31 12:57:26 +0530

    font changed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./style.css";
 
 const App = () => {
   return (
-    <main className="main w-full h-screen bg-black font-[poppins]">
+    <main className="main w-full h-screen bg-black">
       <nav className="navbar w-full h-[10vh] flex items-center justify-center">
         <ul className="flex items-center justify-center gap-14 border-b-[0.5px] border-[#333333] h-full">
           {[
@@ -31,9 +31,9 @@ const App = () => {
       </nav>
       <section className="hero relative w-full h-[90vh]">
         <div className="heading absolute text-white left-1/2 -translate-x-1/2 top-10 flex flex-col items-center gap-2">
-          <h1 id="title" className="text-7xl font-bold tracking-tight">macbook pro</h1>
+          <h1 id="title" className="text-7xl font-extrabold tracking-tight">macbook pro</h1>
           <h2 className="text-xl font-medium">Oh so pro.</h2>
-          <p className="text-sm text-center font-light tracking-tight text-[#e0e0e0] w-3/4 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi non inventore! Impedit, consequuntur. Non, rerum neque. Officia eos ullam facere amet praesentium fugit animi.</p>
+          <p className="text-sm text-center font-light text-[#e0e0e0] w-3/4 mt-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus commodi non inventore! Impedit, consequuntur. Non, rerum neque. Officia eos ullam facere amet praesentium fugit animi.</p>
         </div>
         <Canvas
           className="mac-canvas"
